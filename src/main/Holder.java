1eb800d77794fb4623ad06f99cdd350cfe99b731
VickyVerma709 <vickyverma.v237@gmail>
2021-07-17 23:03:19 +0530

    Java Project

---
+++ src/main/Holder.java
@@ -0,0 +1,12 @@
+package main;
+
+
+
+public class Holder {
+
+	
+	DoubleRoom arr1[] = new DoubleRoom[10]; // Luxury
+	DoubleRoom arr2[] = new DoubleRoom[20]; // Deluxe
+	SingleRoom arr3[] = new SingleRoom[10]; // Luxury
+	SingleRoom arr4[] = new SingleRoom[20]; // Deluxe
+}
---
+++ src/main/NotAvailable.java
@@ -0,0 +1,8 @@
+package main;
+
+public class NotAvailable extends Exception {
+
+	public String toString() {
+		return "Not Available !";
+	}
+}	
